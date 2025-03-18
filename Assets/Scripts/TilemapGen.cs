4ba531390f52b18f6d37b114a3e11d8e4a57c100
Noly5105 <Konrad.Thaisen@gmail.com>
2025-03-18 12:41:05 +0100

    idfk
    a

---
+++ Assets/Scripts/TilemapGen.cs
@@ -0,0 +1,10 @@
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TilemapGen : MonoBehaviour
+{
+    [SerializeField]
+    public List<List<int>> grid = new List<List<int>>();
+}
